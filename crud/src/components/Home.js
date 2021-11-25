935f113b1379840965be8a0a352f4464d0b34617
Hamza <hamxa341@gmail.com>
2021-11-25 17:08:15 +0500

    Done

--- crud/src/components/Home.js
+++ crud/src/components/Home.js
@@ -1,36 +1,47 @@
 import React from 'react'
-import {Table} from 'antd';
+import { Input, Modal, Table } from 'antd';
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
+import { useState, useEffect } from 'react';
 
 
 export default function Home() {
+  const [data, setData] = useState([]);
+  const [isediting, setIsEditing] = useState(false);
+  const [dataEditing, setDataEditing] = useState('')
 
-  const dataSource = [
-    {
-      id: '1',
-      name: 'Hamza',
-      email: 'asd@gmail.com',
-      phone: '1234566789',
-    },
-    {
-      id: '2',
-      name: 'Ubaid',
-      email: 'ubaid@gmail.com',
-      phone: '123555559',
-    },
-    {
-      id: '3',
-      name: 'Mohsin',
-      email: 'mohsin@gmail.com',
-      phone: '1100006789',
-    },
-  ];
 
+// Get Data from Database
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const result = await fetch('http://127.0.0.1:5000/')
+        const getLoadData = await result.json()
+        setData(getLoadData.data)
+      } catch (error) {
+        console.log(error)
+      }
+
+    }
+    getData();
+  }, [])
+
+  // Delete Data from Database
+  const delrow = async (id) => {
+    try {
+      await fetch('http://127.0.0.1:5000/delete/' + id)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+// Columns Name of Database
   const columns = [
     {
       title: 'Id',
       dataIndex: 'id',
       key: 1,
+
     },
     {
       title: 'Name',
@@ -52,17 +63,88 @@ export default function Home() {
       key: 5,
       render: (record) => {
         return <>
-          <EditOutlined />
-          <DeleteOutlined style={{ marginLeft: 15 }} />
+          <EditOutlined onClick={() => editData(record)} />
+          <DeleteOutlined style={{ marginLeft: 15 }} onClick={() => delrow(record.id)} />
         </>
       }
     },
   ];
+// Edit data From Database
+  const [user_name, setUserName] = useState('');
+  const [user_mail, setUserMail] = useState('');
+  const [user_contact, setUserContact] = useState('');
+  const [user_id, setUserId] = useState('');
+
+  const onChangeId = (event) => {
+    setUserId(event.target.value)
+  }
+
+  const onChangeUsername = (event) => {
+    setUserName(event.target.value)
+  }
+
+  const onChangeMail = (event) => {
+    setUserMail(event.target.value)
+  }
+  const onChangeNumber = (event) => {
+    setUserContact(event.target.value)
+  }
+
+  const editData = async (record) => {
+    setIsEditing(true)
+    setDataEditing(record)
+    setUserName(record.name)
+    setUserMail(record.email)
+    setUserContact(record.phone)
+    setUserId(record.id)
+  }
+  const handleOk = async () => {
+    try {
+      const dt = {
+        id : user_id,
+      name: user_name,
+      email: user_mail,
+      phone: user_contact
+    }
+    // console.log(dt)
+    const result = await fetch('http://127.0.0.1:5000/update', {
+      'method': 'POST',
+      headers: {
+      'content-type': 'application/json',
+          },
+          body: JSON.stringify(dt)
+        })
+        // console.log(result)
+      } catch (error) {
+        console.log(error)
+      }
+setIsEditing(false)
+    }
 
 
-    return (
-        <div>
-            <Table dataSource={dataSource} columns={columns} />
-        </div>
-    )
+
+  // console.log(data)
+  return (
+    <div>
+      <Table
+        columns={columns}
+        dataSource={data}
+        rowKey={record => record.id} />
+      <Modal
+        title='Edit Data'
+        visible={isediting}
+        okText="Save Data"
+        onCancel={() => {
+          setIsEditing(false)
+        }}
+        onOk={handleOk} >
+        <Input value={user_id} onChange={onChangeId} disabled={true}/>
+        <Input value={user_name} onChange={onChangeUsername} />
+        <Input value={user_mail} onChange={onChangeMail} />
+        <Input value={user_contact} onChange={onChangeNumber} />
+
+      </Modal>
+    </div>
+  )
+
 }
--- crud/src/components/Open.js
+++ crud/src/components/Open.js
@@ -1,67 +1,72 @@
 import React from 'react'
-import {useState} from 'react'
-import {Button,Form,Input,Modal} from 'antd';
-export default function Open() {
-    const [isModalVisible, setIsModalVisible] = useState(false);
-    const [user_name, setUserName] = useState('');
-    const [user_mail, setUserMail] = useState('');
-    const [user_contact , setUserContact] = useState('');
+import { useState } from 'react'
+import { Button, Form, Input, Modal } from 'antd';
+export default function Open(props) {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [user_name, setUserName] = useState('');
+  const [user_mail, setUserMail] = useState('');
+  const [user_contact, setUserContact] = useState('');
 
-
-    const onChangeUsername = (event) => {
-        setUserName(event.target.value);
-    }
-    const onChangeMail = (event) =>{
-      setUserMail(event.target.value)
-    }
-    const onChangeNumber= (event) =>{
-      setUserContact(event.target.value)
-    }
-
-    const showModal = () => {
-      setIsModalVisible(true);
-    };
-  
-    const handleOk = () => {
-      const res = {
-          user_name: user_name,
-          user_mail : user_mail,
-          user_contact: user_contact,
-
-      }
-      console.log(res)
-      setIsModalVisible(false);
-    };
-  
-    const handleCancel = () => {
-      setIsModalVisible(false);
-    };
-  
-    const NewData = () => (
-      <Form>
-        <Form.Item label='Name' name='fname'>
-          <Input placeholder='Enter Name here' onChange={onChangeUsername} />
-        </Form.Item>
-        <Form.Item label='Email' name='email'>
-          <Input placeholder='Enter Email here' onChange = {onChangeMail}/>
-        </Form.Item>
-        <Form.Item label='Phone No ' name='phone'>
-          <Input placeholder='Enter Phone No here' onChange = {onChangeNumber}></Input>
-        </Form.Item>
-      </Form>
-    )
-  
-    // const handleform = (e) => {
-    //     console.log(e)
-    // }
-    return (
-      <div>
-        <Button type="primary" onClick={showModal}>
-          Add Data
-        </Button>
-        <Modal title="Add Data" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} >
-          <NewData />
-        </Modal>
-      </div>
-    )
+  const onChangeUsername = (event) => {
+    setUserName(event.target.value);
   }
+  const onChangeMail = (event) => {
+    setUserMail(event.target.value)
+  }
+  const onChangeNumber = (event) => {
+    setUserContact(event.target.value)
+  }
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = async () => {
+    try {
+      const dt = {
+        name: user_name,
+        email: user_mail,
+        phone: user_contact
+      }
+      const result = await fetch('http://127.0.0.1:5000/insert', {
+        'method': 'POST',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify(dt)
+      })
+      console.log(result)
+    } catch (error) {
+      console.log(error)
+    }
+    setIsModalVisible(false);
+  }
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  // const handleform = (e) => {
+  //     console.log(e)
+  // }
+  return (
+    <div>
+      <Button type="primary" onClick={showModal}>
+        Add Data
+      </Button>
+      <Modal title="Add Data" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} >
+        <Form>
+          <Form.Item label='Name' name='name'>
+            <Input placeholder='Enter Name here' onChange={onChangeUsername} />
+          </Form.Item>
+          <Form.Item label='Email' name = 'email'>
+            <Input placeholder='Enter Email here' onChange={onChangeMail} />
+          </Form.Item>
+          <Form.Item label='Phone No ' name = 'phone  '>
+            <Input placeholder='Enter Phone No here' onChange={onChangeNumber}></Input>
+          </Form.Item>
+        </Form>
+      </Modal>
+    </div>
+  )
+}
